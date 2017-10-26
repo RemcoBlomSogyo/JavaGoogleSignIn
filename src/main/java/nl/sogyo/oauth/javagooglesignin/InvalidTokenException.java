acbf2fff68abc816b0097f0f71fa254e6bbfa126
Remco Blom <rblom@sogyo.nl>
2017-10-26 16:40:59 +0200

    changed google sign in, now gets only id token instead of access token

---
+++ src/main/java/nl/sogyo/oauth/javagooglesignin/InvalidTokenException.java
@@ -0,0 +1,15 @@
+package nl.sogyo.oauth.javagooglesignin;
+
+/**
+ * The InvalidTokenException class is a checked exception that 
+ * can be thrown if the Google token verifier returns null and 
+ * IOException and GeneralSecurityException has not been thrown.
+ */
+public class InvalidTokenException extends Exception {
+
+	private static final long serialVersionUID = 3655022714071380777L;
+
+	public InvalidTokenException() {
+		super("Token verifier returned null. The token is invalid");
+	}
+}
--- src/main/java/nl/sogyo/oauth/javagooglesignin/OauthHelper.java
+++ src/main/java/nl/sogyo/oauth/javagooglesignin/OauthHelper.java
@@ -3,11 +3,13 @@ package nl.sogyo.oauth.javagooglesignin;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
@@ -19,104 +21,61 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 public class OauthHelper {
 	
 	private String clientId;
-	private String clientSecret;
-	private String tokenServerEncodedUrl;
-	private String redirectUri;
-	private static final String DEFAULT_TOKEN_SERVER_ENCODED_URL = "https://www.googleapis.com/oauth2/v4/token";
 	
 	/**
 	 * Initializes the OauthHelper class.
-	 * The constructor needs a file object that leads to a JSON file with the client id
-	 * and client secret to obtain user information of Google. To download the JSON file,
-	 * go to console.developer.google.com and create an OAuth 2.0 client ID. The second parameter
-	 * is the redirect URI that the user goes to when the login succeeds. The URI has to be configured
-	 * at the OAuth client ID in the developer console.
+	 * The constructor needs a file object that leads to a client secret JSON file 
+	 * that has a client id. The client id is required to validate the id token at Google.
+	 * To download the JSON file, go to console.developer.google.com and create an OAuth 2.0 client id. 
 	 * 
 	 * @param clientSecretJson
-	 * @param redirectUri
 	 * @throws IOException
 	 */
-	public OauthHelper(File clientSecretJson, String redirectUri) throws IOException {
-		this(clientSecretJson, DEFAULT_TOKEN_SERVER_ENCODED_URL, redirectUri);
-	}
-	
-	/**
-	 * Initializes the OauthHelper class.
-	 * The constructor needs a file object that leads to a JSON file with the client id
-	 * and client secret to obtain user information of Google. To download the JSON file,
-	 * go to console.developer.google.com and create an OAuth 2.0 client ID. The third parameter
-	 * is the redirect URI that the user goes to when the login succeeds. The URI has to be configured
-	 * at the OAuth client ID in the developer console. This constructor needs also a token server
-	 * encoded url. The default url is https://www.googleapis.com/oauth2/v4/token.
-	 * 
-	 * @param clientSecretJson
-	 * @param tokenServerEncodedUrl
-	 * @param redirectUri
-	 * @throws IOException
-	 */
-	public OauthHelper(File clientSecretJson, String tokenServerEncodedUrl, String redirectUri) throws IOException {
+	public OauthHelper(File clientSecretJson) throws IOException {
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), 
 				new FileReader(clientSecretJson));
 		clientId = clientSecrets.getDetails().getClientId();
-		clientSecret = clientSecrets.getDetails().getClientSecret();
-		this.tokenServerEncodedUrl = tokenServerEncodedUrl;
-		this.redirectUri = redirectUri;
 	}
 	
 	/**
 	 * Initializes the OauthHelper class.
-	 * The constructor needs a client id and client secret to obtain user information of Google. 
-	 * To get the client id and client secret, go to console.developer.google.com and create an 
-	 * OAuth 2.0 client ID. The third parameter is the redirect URI that the user goes to when the 
-	 * login succeeds. The URI has to be configured at the OAuth client ID in the developer console. 
+	 * The constructor needs a client id string. The client id is required to validate the id token 
+	 * at Google. To get a client id, go to console.developer.google.com and create an OAuth 2.0 client id.
 	 * 
 	 * @param clientId
-	 * @param clientSecret
 	 * @param redirectUri
 	 */
-	public OauthHelper(String clientId, String clientSecret, String redirectUri) {
-		this(clientId, clientSecret, DEFAULT_TOKEN_SERVER_ENCODED_URL, redirectUri);
-	}
-	
-	/**
-	 * Initializes the OauthHelper class.
-	 * The constructor needs a client id and client secret to obtain user information of Google. 
-	 * To get the client id and client secret, go to console.developer.google.com and create an 
-	 * OAuth 2.0 client ID. This constructor needs also a token server encoded url. The default url 
-	 * is https://www.googleapis.com/oauth2/v4/token. The fourth parameter is the redirect URI that 
-	 * the user goes to when the login succeeds. The URI has to be configured at the OAuth client ID 
-	 * in the developer console. 
-	 * 
-	 * @param clientId
-	 * @param clientSecret
-	 * @param tokenServerEncodedUrl
-	 * @param redirectUri
-	 */
-	public OauthHelper(String clientId, String clientSecret, String tokenServerEncodedUrl, String redirectUri) {
+	public OauthHelper(String clientId) {
 		this.clientId = clientId;
-		this.clientSecret = clientSecret;
-		this.tokenServerEncodedUrl = tokenServerEncodedUrl;
-		this.redirectUri = redirectUri;
 	}
 	
 	/**
 	 * Gets a token from Google and creates a Google user object with the user ID, given name, family
 	 * name and email address from it.
 	 * 
-	 * @param authorizationCode
+	 * Takes an id token and verifies it at Google.
+	 * It returns a GoogleUser object that is made of the payload of the id token.
+	 * 
+	 * @param idTokenString
 	 * @return
 	 * @throws IOException
+	 * @throws GeneralSecurityException
 	 * @throws EmailNotVerifiedException
+	 * @throws InvalidTokenException
 	 */
-	public GoogleUser getUserFromGoogle(String authorizationCode) throws IOException, EmailNotVerifiedException {
-		GoogleTokenResponse tokenResponse = new GoogleAuthorizationCodeTokenRequest(
-				new NetHttpTransport(), JacksonFactory.getDefaultInstance(),
-				tokenServerEncodedUrl, clientId, clientSecret, authorizationCode,
-				redirectUri).execute();
-		GoogleIdToken idToken = tokenResponse.parseIdToken();
-		GoogleIdToken.Payload payload = idToken.getPayload();
-		GoogleUser user = new GoogleUser(payload);
-		return user;
+	public GoogleUser getUserFromToken(String idTokenString) throws IOException, GeneralSecurityException, 
+			EmailNotVerifiedException, InvalidTokenException {
+		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), 
+				JacksonFactory.getDefaultInstance()).setAudience(Collections.singletonList(clientId)).build();
+
+		GoogleIdToken idToken = verifier.verify(idTokenString);
+
+		if (idToken != null) {
+		  Payload payload = idToken.getPayload();
+		  GoogleUser user = new GoogleUser(payload);
+		  return user;
+		} else {
+		  throw new InvalidTokenException();
+		}
 	}
 }
-
